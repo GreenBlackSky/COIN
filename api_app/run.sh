79e7f1a7218989de28a686b74a250d6749d13fb2
GreenBlackSky <greenblacksky@gmail.com>
2021-01-01 19:01:26 +0300

    connection errors

--- api_app/run.sh
+++ api_app/run.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# Check if rabbit is up and running before starting the service.
-
 until nc -z ${RABBIT_HOST} ${RABBIT_PORT}; do
     echo "$(date) - waiting for rabbitmq..."
     sleep 2
 done
-
-# Run Service
 
 gunicorn -b 0.0.0.0:5000 app:app
--- cache_app/run.sh
+++ cache_app/run.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Check if rabbit is up and running before starting the service.
-
 until nc -z ${RABBIT_HOST} ${RABBIT_PORT}; do
     echo "$(date) - waiting for rabbitmq..."
     sleep 2
 done
 
-# Run Migrations
-
-# alembic upgrade head
-
-# Run Service
-
-nameko run --config config.yml cache_service.service --backdoor 3000
+nameko run --config /app/config.yaml app.cache_service --backdoor 3000
