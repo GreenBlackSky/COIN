103b7db4e1f55b5481b5758d3715e080c6892be9
GreenBlackSky <greenblacksky@gmail.com>
2021-06-28 23:40:41 +0300

    build fixed

--- core_db/docker-entrypoint-initdb.d/init.sql
+++ core_db/docker-entrypoint-initdb.d/init.sql
@@ -9,14 +9,14 @@ CREATE TABLE events (
     id SERIAL PRIMARY KEY,
     event_time TIMESTAMP NOT NULL,
     diff INT NOT NULL,
-    description VARCHAR(200) NOT NULL,
+    description VARCHAR(200) NOT NULL
     -- confirmed BOOLEAN NOT NULL
 );
 CREATE TABLE save_points (
     id SERIAL PRIMARY KEY,
     datetime TIMESTAMP NOT NULL,
     total FLOAT NOT NULL
-)
+);
 CREATE TABLE labels (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
--- core_service/app/__init__.py
+++ core_service/app/__init__.py
@@ -1,3 +1,4 @@
+from common.celery_utils import celery_app
 from .handlers.account import *
 from .handlers.event import *
 from .handlers.label import *
