6d2a7baab8813de727c398638fcf731664cdc5bf
GreenBlackSky <greenblacksky@gmail.com>
2022-02-12 00:45:33 +0300

    choosing period

--- web_entrypoint/lib/views/widgets/topper.dart
+++ web_entrypoint/lib/views/widgets/topper.dart
@@ -1,9 +1,27 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
 import 'package:coin_client/storage.dart';
 import 'common/common.dart';
+
+void _setDateRange(BuildContext context, DateTimeRange range) {
+  if (range == null) {
+    return;
+  }
+  storage.periodStart = range.start;
+  storage.periodEnd = range.end;
+  Navigator.pushNamed(context, "/loading",
+      arguments: LoadingArgs(LoadingType.SYNC_DATA));
+}
+
+String _buildPeriodStr() {
+  String startYear = storage.periodStart.year.toString();
+  String startMonth = storage.periodStart.month.toString().padLeft(2, '0');
+  String startDay = storage.periodStart.day.toString().padLeft(2, '0');
+  String endYear = storage.periodEnd.year.toString();
+  String endMonth = storage.periodEnd.month.toString().padLeft(2, '0');
+  String endDay = storage.periodEnd.day.toString().padLeft(2, '0');
+  return "$startYear-$startMonth-$startDay - $endYear-$endMonth-$endDay";
+}
 
 class Topper extends AppBar {
   Topper(BuildContext context)
@@ -13,7 +31,7 @@ class Topper extends AppBar {
           buildButton(storage.accounts[storage.accountIndex]['name'], () {
             Navigator.pushNamed(context, "/accounts_view");
           }),
-          buildButton("${storage.periodStart} - ${storage.periodEnd}", () {
+          buildButton(_buildPeriodStr(), () {
             showDateRangePicker(
                 context: context,
                 firstDate: storage.periodStart,
@@ -27,7 +45,9 @@ class Topper extends AppBar {
                     child: child,
                     widthFactor: 0.5,
                   );
-                });
+                }).then((range) {
+              _setDateRange(context, range);
+            });
           })
         ])) {}
 }
